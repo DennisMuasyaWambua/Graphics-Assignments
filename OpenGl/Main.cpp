d2bbb8ec05c334101cda3e0bc12b59df6ade2288
StevenMburu1 <steven.mburu@strathmore.edu>
2021-11-22 11:51:57 +0300

    Arc

--- OpenGl/Main.cpp
+++ OpenGl/Main.cpp
@@ -1,21 +1,21 @@
 #include<GL/glut.h>
 #include <math.h>
 
-
+ 
 void DrawHalfCircle()
-
-{ 
+{    
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT); 
 	int number = 20;
-	float radius = 0.4f;
+	float radius = 0.4f; 
 	float twopi =1.05* 3.142;
 	glMatrixMode(GL_MODELVIEW); //operate in model view
 	glLoadIdentity();//after every matrixmode, loadidentity is used.
 
 	glBegin(GL_LINE_STRIP);
 	glColor3f(0.0f, 1.0f, 0.0f);
-	//glVertex2f(0.0f, 0.0f);
+	//glVertex2f(0.0f, 0.0f); 
+
 	for (int i = 0; i < 20; i++)
 	{
 		glVertex2f(radius * cosf(i * twopi / number), radius * sinf(i * twopi / number));
